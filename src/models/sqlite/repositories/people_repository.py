bd387a7b715885a934b13eddd2fbef71931f4bc5
xManoelx <manoeldr03@gmail.com>
2025-11-25 20:42:55 -0300

    feat: implementing people repository

---
+++ src/models/sqlite/repositories/people_repository.py
@@ -0,0 +1,48 @@
+from sqlalchemy.orm.exc import NoResultFound
+from src.models.sqlite.entities.people import PeopleTable
+from src.models.sqlite.entities.pets import PetsTable
+
+class PeopleRepository:
+    
+    # Metodo construtor
+    def __init__(self, db_connection) -> None:
+        self.__db_connection = db_connection
+
+    # Funcao para inserir uma nova pessoa
+    def insert_person(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
+        """Insere uma nova pessoa"""
+        with self.__db_connection as database:
+            try:
+                person_data = PeopleTable(
+                    first_name=first_name, 
+                    last_name=last_name, 
+                    age=age,
+                    pet_id=pet_id
+                )
+                database.session.add(person_data)
+                database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+
+    # Funcao para listar todas as pessoas
+    def get_person (self, person_id: int) -> PeopleTable:
+        """Retorna uma pessoa pelo ID"""
+        with self.__db_connection as database:
+            try: 
+                person = (
+                    database.session
+                    .query(PeopleTable)
+                    .outerjoin(PetsTable, PetsTable.id == PeopleTable.id)
+                    .filter(PeopleTable.id == person_id)
+                    .with_entities(
+                        PeopleTable.first_name,
+                        PeopleTable.last_name,
+                        PetsTable.name.label('pet_name'),
+                        PetsTable.type.label('pet_type')
+                    )
+                    .one()
+                )
+                return person
+            except NoResultFound:
+                return None
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,8 +1,9 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
-# db_connection_handler.connect_to_db()
+db_connection_handler.connect_to_db()
 
 # Teste para listar pets
 @pytest.mark.skip(reason="interacao com o banco de dados")
@@ -13,8 +14,33 @@ def test_list_pets():
     print(response)
 
 # Teste para deletar um pet
+@pytest.mark.skip(reason="interacao com o banco de dados")
 def test_delete_pets():
     name = "belinha"
 
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+# Teste para inserir uma pessoa
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_insert_person():
+    first_name = "Ricardo"
+    last_name = "Costa"
+    age = 30
+    pet_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+# Teste para inserir uma pessoa
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print('----------------------------------------')
+    print(response)
+    print(response.pet_name)
+    print('----------------------------------------')
